9c8c264f1071bb0669858ef513658ff4fd24378b
Mykola Maik <mykoluamaik@gmail.com>
2024-01-11 15:02:35 +0200

    Implement these compoonents

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Markdown from './components/Markdown';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Grid
+      item
+      xs={12}
+      md={8}
+      sx={{
+        '& .markdown': {
+          py: 3,
+        },
+      }}
+    >
+      <Header />
+      <Typography variant="h6" gutterBottom>
+        Integrating google maps into react application
+      </Typography>
+      <Divider />
+      <Markdown className="markdown">
+        This is markdown
+      </Markdown>
+      <Footer 
+        description='Made by Mykola Maik'
+      />
+    </Grid>
+  );
 }
-
-export default App
